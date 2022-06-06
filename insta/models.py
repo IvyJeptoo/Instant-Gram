5570c9bf34e5af62dcdd71e303c9959ac416f0ff
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-06-07 01:45:49 +0300

    create models

--- insta/models.py
+++ insta/models.py
@@ -1,48 +1,101 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
+from cloudinary.models import CloudinaryField
 
 
-# user, profile, post, comments
+class Image(models.Model):
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='images')
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)    
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length=30)
+    image = CloudinaryField('image')
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-pk"]
 
 
+    @classmethod
+    def images(cls):
+        images = cls.objects.all()
+        return images
+
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+        
+    
+
+    @classmethod
+    def update_image(cls,old,new):
+        cap = Image.objects.filter(caption=old).update(caption=new)
+        return cap
+
+    def __str__(self):
+        return self.name
+
+    
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio=models.TextField()
-    profile_picture=models.ImageField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
     
+    bio = models.CharField(max_length=300)
+    name = models.CharField(blank=True, max_length=120)
+    photo = CloudinaryField('image')
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    @classmethod
+    def profile(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+    def photo_url(self):
+        if self.photo and hasattr(self.photo, 'url'):
+            return self.photo.url
+
+    def save_profile(self):
+        self.user
+
     def __str__(self):
-        return self.bio
-    
-    
-class Post(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    image=models.ImageField()
-    caption=models.TextField(max_length=100)
-    # likes=models.ForeignKey('Likes',on_delete=models.SET_NULL)
-    date=models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.caption
-    
-    
-class Comments(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    post=models.ForeignKey(Post,on_delete=models.CASCADE)
-    comment=models.TextField(max_length=200)
-    
-    
-    def __str__(self):
-        return self.comment
-    
+        return self.name
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
+
 class Follow(models.Model):
-    follower = models.ForeignKey(User, on_delete=models.CASCADE,related_name='follower')
-    total_followings= models.ForeignKey(User, on_delete=models.CASCADE,related_name='followers')
-    
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
     def __str__(self):
-        return self.follower
-    
-    
+        return f'{self.follower} Follow'
 
+class Comment(models.Model):
+    comment = models.TextField()
+    user = models.ForeignKey('Profile',on_delete=models.CASCADE,related_name='comment')
+    photo = models.ForeignKey('Image',on_delete=models.CASCADE,related_name='comment')
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def __str__(self):
+        return f'{self.user.name} Image'
     
-
-
