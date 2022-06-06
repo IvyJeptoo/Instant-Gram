5c12470cc5663b9e4136d0d25a68ad141726c20e
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-06-07 02:06:26 +0300

    edit url paths

--- insta/urls.py
+++ insta/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('home', views.home),
-    path('sign-up', views.sign_up),
-    # path('login', views.login)
+
+urlpatterns=[
+    path('',views.index,name = 'index'),
+    path('home', views.index),
+    path('signup', views.sign_up),    
+    path('profile/<str:username>/',views.profile,name='profile'),
+    path('edit/profile/',views.update_profile,name='update'),
+    path('image/',views.post,name='post'),
+    path('search/', views.search_profile, name='search'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow'),
+    path('image/<id>', views.comment, name='comment'),
+    path('like/<id>', views.like_post,name='like'),
+    
 ]
